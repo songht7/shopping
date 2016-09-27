77ad1a2d489dbde605c237b9a4b4f9ea0fdc0a99
songht7 <songht1109@sina.com>
2016-09-27 23:16:00 +0800

    #二级菜单

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,13 +1,25 @@
 $(function(){
     $("#MenuBtn").on("click",function(){
-        var slideMenu=$("#SlideMenu");
+        var obj=$(this), slideMenu=$("#SlideMenu");
         if(!slideMenu.hasClass("on")){
+            obj.addClass("active");
             slideMenu.addClass("on");
             slideMenu.animate({"height":"30px","opacity":1,"padding":"20px 0 0"},500);
         }else{
+            obj.removeClass("active");
             slideMenu.removeClass("on");
             slideMenu.animate({"height":"0","opacity":"0","padding":"0"},500);
         }
+    });
+    $(".multi").mouseover(function(){
+        var obj=$(this);
+        obj.find(".hasSub").addClass("active");
+        obj.find(".subMenu").show();
+    });
+    $(".multi").mouseleave(function(){
+        var obj=$(this);
+        obj.find(".hasSub").removeClass("active");
+        obj.find(".subMenu").hide();
     });
     $("#Language").change(function(){
         var obj=$(this);
@@ -20,5 +32,28 @@ $(function(){
                 window.location.reload() 
             }
         });
+    });
+    //////
+    function getGallerySlideNumber(){
+        var scrollOffset = Math.abs($(".homeSlide .wrapper").offset().left);
+        var slideWidth = $(".homeSlide .slides").width();
+        var slideNum = parseInt(scrollOffset/slideWidth);
+        return slideNum;
+    }
+
+    $(".homeSlide > .arrowLeft").click(function(){
+        var gallery =  $(".homeSlide");
+        var prevSlideNum = getGallerySlideNumber() - 1; 
+        if(prevSlideNum >= 0){
+            gallery.find(".swiper-wrapper").scrollTo(gallery.find('.slides:eq('+prevSlideNum+')'), 500);
+        }
+    });
+
+    $(".homeSlide > .arrowRight").click(function(){
+        var gallery =  $(".homeSlide");
+        var nextSlideNum = getGallerySlideNumber() + 1;
+        if(nextSlideNum <= gallery.find('.slides').length){
+            gallery.find(".swiper-wrapper").scrollTo(gallery.find('.slides:eq('+nextSlideNum+')'), 500);
+        }
     });
 });
