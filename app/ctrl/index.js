bd27f64bb5a8128c0467ec008d5c5a5ab1d4d133
songht7 <songht1109@sina.com>
2016-09-29 00:16:09 +0800

    # home ctg list

--- app/ctrl/index.js
+++ app/ctrl/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var langpack=require('../module/lang-pack.js');
+var ctg=require('../module/category.js');
 
 
 exports.index = function(getlang,req, res, next) {
 	var langs=langpack;
 	var menu=langs.menu,
 	footer_menu=langs.footer_menu;
-	//console.log(footer_menu);
+	var category=ctg.product;
+	console.log(category);
 	var lgkey=getlang.key,lgtype=getlang.lgType;
-  res.render('index', { title: 'shopping',lang:lgtype,lgkey:lgkey,langs:langs,menu:menu,footerMenu:footer_menu});
+  res.render('index', { title: 'shopping',lang:lgtype,lgkey:lgkey,langs:langs,menu:menu,category:category,footerMenu:footer_menu});
 }
---
+++ app/module/category.js
@@ -0,0 +1,100 @@
+////["类型","英","中"]
+///////[0,1,2]
+var category={
+	"product":[
+		/**pro1**/
+		{
+			"colNumb":3,
+			"ctgName":["c1title","Latest From Your Favorite Bloggers","最喜欢的博客"]
+			,"ctgList":[
+				{
+					"id":1,
+					"ctgTitle":["pname","Matthew Rhys Suckers Joe! ","Matthew 乔！"],
+					"ctgOvs":["oname","The Wine Show","葡萄酒展"]
+				},
+				{
+					"id":2,
+					"ctgTitle":["pname","Finding Inspiration","寻找灵感"],
+					"ctgOvs":["oname","Wendy’s Lookbook","温迪的代言人"]
+				},
+				{
+					"id":3,
+					"ctgTitle":["pname","How to Travel for a Living","如何为生活旅行"],
+					"ctgOvs":["oname","High On Life","高生活"]
+				},
+				{
+					"id":4,
+					"ctgTitle":["pname","Matthew Rhys Suckers Joe! ","Matthew 乔！"],
+					"ctgOvs":["oname","The Wine Show","葡萄酒展"]
+				},
+				{
+					"id":5,
+					"ctgTitle":["pname","Matthew Rhys Suckers Joe! ","Matthew 乔！"],
+					"ctgOvs":["oname","The Wine Show","葡萄酒展"]
+				},
+				{
+					"id":6,
+					"ctgTitle":["pname","Matthew Rhys Suckers Joe! ","Matthew 乔！"],
+					"ctgOvs":["oname","The Wine Show","葡萄酒展"]
+				}
+			]
+		}
+		/**pro2**/
+		,{
+			"colNumb":4,
+			"ctgName":["c1title","Just For You","只为你"]
+			,"ctgList":[
+				{
+					"id":7,
+					"ctgTitle":["pname","Confessions of a Food Stylist","食品设计师的告白"],
+					"ctgOvs":["oname","Learning ZoneXpress","学习ZoneXpress"]
+				},
+				{
+					"id":8,
+					"ctgTitle":["p2name","An artistic view on Nittardi","nittardi艺术观"],
+					"ctgOvs":["o2name","The Wine Show","葡萄酒展"]
+				},
+				{
+					"id":9,
+					"ctgTitle":["p3name","Be your shoeself","是你的shoeself"],
+					"ctgOvs":["o3name","Wendy's Lookbook","温迪的代言人"]
+				},
+				{
+					"id":10,
+					"ctgTitle":["p4name","MUJI SG Interior Advisor Service","无印良品SG内部顾问服务"],
+					"ctgOvs":["o5name","MUJI SG","无印良品SG"]
+				}
+			]
+		}
+		/**pro3**/
+		,{
+			"colNumb":4,
+			"ctgName":["c1title","Newly Added","新添加的"]
+			,"ctgBrief":["cf","latest most viewed videos","最新最受欢迎视频"]
+			,"ctgList":[
+				{
+					"id":11,
+					"ctgTitle":["pname","Outtakes Bloopers","镜头花絮"],
+					"ctgOvs":["oname","The Wine Show","葡萄酒展"]
+				},
+				{
+					"id":12,
+					"ctgTitle":["p2name","Color your cracked Iphone","苹果手机"],
+					"ctgOvs":["o2name","Jessika Messy","杰西卡凌乱"]
+				},
+				{
+					"id":13,
+					"ctgTitle":["p3name","The Top Five Secrets of a Successful Restaurant","成功餐厅的前五大秘密"],
+					"ctgOvs":["o3name","Bloomberg ","彭博社"]
+				},
+				{
+					"id":14,
+					"ctgTitle":["p4name","Pairings: Winter Fashion","冬季时装"],
+					"ctgOvs":["o5name","Wendy's Lookbook","温迪的代言人"]
+				}
+			]
+		}
+
+	]
+};
+module.exports = category;
